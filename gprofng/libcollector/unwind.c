5e5902603ac24f3e9247bda912c31a21a62360c4
XYenChi <oriachiuan@gmail.com>
2024-05-22 18:17:09 +0800

    change uc_mcontext stack pointer register calling convention according to glibc

--- gprofng/libcollector/unwind.c
+++ gprofng/libcollector/unwind.c
@@ -237,13 +237,20 @@ memory_error_func (int status ATTRIBUTE_UNUSED, bfd_vma addr ATTRIBUTE_UNUSED,
 	    context->uc_stack.ss_size = 0x100000; \
 	}
 
-#elif ARCH(Aarch64) || ARCH(RISCV)
+#elif ARCH(Aarch64) 
 #define FILL_CONTEXT(context) \
     { CALL_UTIL (getcontext) (context);  \
       context->uc_mcontext.sp = (__u64) __builtin_frame_address(0); \
     }
 
-#endif /* ARCH() */
+#elif ARCH(RISCV)
+#define FILL_CONTEXT(context) \
+    { \
+        CALL_UTIL(getcontext)(context); \
+        context->uc_mcontext.__gregs[2] = (__u64)__builtin_frame_address(0); \
+	}
+
+#endif/* ARCH() */
 
 static int
 getByteInstruction (unsigned char *p)
